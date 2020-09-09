4427f4deca5f1c25ab204187330bae2393b62187
Frank Ho <58057620+frank-quoc@users.noreply.github.com>
2020-09-09 14:22:32 -0700

    Create answer.py

---
+++ python_intro/function/project/answer.py
@@ -0,0 +1,43 @@
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+
+bomb_mass = 1
+
+# Turn up the Temperature
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32)*(5/9)
+  return c_temp
+
+def c_to_f(c_temp):
+  f_temp = c_temp*(9/5) + 32
+
+f100_in_celsius = f_to_c(100)
+c0_in_fahrenheit = c_to_f(0)
+
+# Use the Force
+def get_force(mass, acceleration):
+  return mass*acceleration
+
+train_force = get_force(train_mass, train_acceleration)
+print(train_force)
+
+X = train_force
+print("The GE train supplies", X, "Newtons of force.")
+
+def get_energy(mass, c=3*10**8):
+  return mass*c**2
+
+bomb_energy = get_energy(bomb_mass)
+
+X = bomb_energy
+print("A 1kg bomb supplies", X, "Joules.")
+
+# Do the Work
+def get_work(mass, acceleration, distance):
+  force = get_force(mass, acceleration)
+  return force*distance
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+
+print("The GE train does", train_work, "Joules of work over", train_distance, "meters.")

7a8c3605af9868e2fe968a5466800c4acc125090
Frank Ho <58057620+frank-quoc@users.noreply.github.com>
2020-09-09 16:06:05 -0700

    Create answer.py

---
+++ python_intro/control_flow/article_advanced/answer.py
@@ -0,0 +1,76 @@
+"""1. In Range"""
+# Write your in_range function here:
+def in_range(num, lower, upper):
+  if (num >= lower) and (num <= upper):
+    return True
+  return False
+
+# Uncomment these function calls to test your in_range function:
+print(in_range(10, 10, 10))
+# should print True
+print(in_range(5, 10, 20))
+# should print False
+
+"""2. Same Name"""
+# Write your same_name function here:
+def same_name(your_name, my_name):
+  if your_name == my_name:
+    return True
+  return False
+
+# Uncomment these function calls to test your same_name function:
+print(same_name("Colby", "Colby"))
+# should print True
+print(same_name("Tina", "Amber"))
+# should print False
+
+"""3. Always False"""
+# Write your always_false function here:
+def always_false(num):
+  if (num >= 0) or (num <=0):
+    return False
+
+# Uncomment these function calls to test your always_false function:
+print(always_false(0))
+# should print False
+print(always_false(-1))
+# should print False
+print(always_false(1))
+# should print False
+
+"""4. Movie Review"""
+# Write your movie_review function here:
+def movie_review(rating):
+  if rating <= 5:
+    return "Avoid at all costs!"
+  elif rating < 9:
+    return "This one was fun."
+  else:
+    return "Outstanding!"
+
+# Uncomment these function calls to test your movie_review function:
+print(movie_review(9))
+# should print "Outstanding!"
+print(movie_review(4))
+# should print "Avoid at all costs!"
+print(movie_review(6))
+# should print "This one was fun."
+
+"""5. Max Number"""
+# Write your max_num function here:
+def max_num(num1, num2, num3):
+  max_n = max(num1, num2, num3)
+  
+  if (max_n == num1 and max_n == num2) or (max_n == num1 and max_n == num3) or (max_n == num2 and max_n == num3):
+    return "It's a tie!"
+  return max_n
+
+# Uncomment these function calls to test your max_num function:
+print(max_num(-10, 0, 10))
+# should print 10
+print(max_num(-10, 5, -30))
+# should print 5
+print(max_num(-5, -10, -10))
+# should print -5
+print(max_num(2, 3, 3))
+# should print "It's a tie!"

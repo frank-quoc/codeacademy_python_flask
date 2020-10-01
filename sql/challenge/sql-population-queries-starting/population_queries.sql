c4f0a1de3b15d66aea480470e909469298b9ba19
Frank Ho <58057620+frank-quoc@users.noreply.github.com>
2020-09-30 18:50:28 -0700

    Update population_queries.sql

--- sql/challenge/sql-population-queries-starting/population_queries.sql
+++ sql/challenge/sql-population-queries-starting/population_queries.sql
@@ -4,3 +4,34 @@ SELECT DISTINCT year from population_years;
 
 -- Add your additional queries below:
 
+-- 4: 1.54526 million
+SELECT population
+FROM population_years
+WHERE country = 'Gabon'
+ORDER BY population DESC
+LIMIT 1;
+
+
+-- 5: 
+SELECT country
+FROM population_years
+WHERE year = 2005
+ORDER BY population ASC
+LIMIT 10;
+
+-- 6: 
+SELECT DISTINCT country
+FROM population_years
+WHERE population > 100
+  AND year = 2010;
+
+-- 7:
+SELECT DISTINCT country
+FROM population_years
+WHERE country LIKE '%Islands%';
+
+-- 8: 240.27152 - 214.67661 = 25.59491
+SELECT population
+FROM population_years
+WHERE country = 'Indonesia'
+  AND year BETWEEN 2000 AND 2010;

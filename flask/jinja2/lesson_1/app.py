305b248a153df9ac74e1b6d08a3f3a3dc568adc4
Frank Ho <58057620+frank-quoc@users.noreply.github.com>
2021-02-17 11:21:53 -0800

    added view function: about

--- flask/jinja2/lesson_1/app.py
+++ flask/jinja2/lesson_1/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template
 from helper import recipes, descriptions, ingredients, instructions
 
@@ -7,6 +8,11 @@ app = Flask(__name__)
 def index():
   return render_template("index.html", template_recipes=recipes)
 
+@app.route('/about')
+def about():
+  return render_template("about.html")
+
 @app.route("/recipe/<int:id>")
 def recipe(id):
-  return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id])
+  return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], 
+                         template_ingredients=ingredients[id], template_instructions=instructions[id])
